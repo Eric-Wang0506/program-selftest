78fc86afb4a5253e5f44532ac1d5c6ddecdb549c
Eric Wang <72305037+Eric-Wang0506@users.noreply.github.com>
2021-08-10 20:57:20 +0800

    Add files via upload

---
+++ check.bat
@@ -0,0 +1,91 @@
+@echo off
+echo Test Case: 1
+time<enter
+"awake.exe" <"awake1.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake1.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 2
+time<enter
+"awake.exe" <"awake2.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake2.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 3
+time<enter
+"awake.exe" <"awake3.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake3.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 4
+time<enter
+"awake.exe" <"awake4.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake4.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 5
+time<enter
+"awake.exe" <"awake5.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake5.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 6
+time<enter
+"awake.exe" <"awake6.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake6.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 7
+time<enter
+"awake.exe" <"awake7.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake7.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 8
+time<enter
+"awake.exe" <"awake8.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake8.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 9
+time<enter
+"awake.exe" <"awake9.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake9.out" /W
+pause
+del _tmpout
+echo.
+
+echo Test Case: 10
+time<enter
+"awake.exe" <"awake10.in" >"_tmpout"
+time<enter
+fc "_tmpout" "awake10.out" /W
+pause
+del _tmpout
+echo.
+
